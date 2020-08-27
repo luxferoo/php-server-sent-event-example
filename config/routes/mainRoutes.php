833e4c87e1df74125f845ecc2a9d599257ca4c87
imam harir <imamharir@gmail.com>
2020-08-27 14:50:24 +0100

    Update mainRoutes.php
    redirect / to /home

--- config/routes/mainRoutes.php
+++ config/routes/mainRoutes.php
@@ -4,6 +4,9 @@ use App\Service\Router\Router;
 
 /** @var Router $router */
 $router->get("/home", "Main#index","home");
+$router->get("/", function (){
+    header('location: /home');
+});
 
 $router->addProxy('/home',function ($url, &$matches){
     if(!isset($_SESSION['connected_member'])){
